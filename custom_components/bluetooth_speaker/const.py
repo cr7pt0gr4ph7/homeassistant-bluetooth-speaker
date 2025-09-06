b7ae0219456f64f94117ff4f2866200ce5062973
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-09-06 11:39:36 +0000

    First draft of connection management using bleak

--- custom_components/bluetooth_speaker/const.py
+++ custom_components/bluetooth_speaker/const.py
@@ -1,3 +1,7 @@
 """Constants for component."""
 
 DOMAIN = "bluetooth_speaker"
+
+# How often to check whether the Bluetooth device is still available
+# when not currently connected.
+DEFAULT_SCAN_INTERVAL = 30
--- custom_components/bluetooth_speaker/coordinator.py
+++ custom_components/bluetooth_speaker/coordinator.py
@@ -1,9 +1,88 @@
 """Support for Bluetooth speakers."""
 
+from __future__ import annotations
+
+from datetime import timedelta
+import logging
+
+from bleak import BleakClient
+from bleak.backends.device import BLEDevice
+from bleak_retry_connector import establish_connection, BleakClientWithServiceCache
+
+from homeassistant.components import bluetooth
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import DEFAULT_SCAN_INTERVAL, DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 type BluetoothSpeakerConfigEntry = ConfigEntry[BluetoothSpeakerCoordinator]
 
-class BluetoothSpeakerCoordinator:
+
+class BluetoothSpeakerDevice:
+    """Class representing the bluetooth speaker."""
+    ble_device: BLEDevice
+    client: BleakClient
+
+    def __init__(self, ble_device: BLEDevice):
+        """Initialize the device representation."""
+        self.ble_device = ble_device
+        self.client = BleakClient(ble_device)
+
+    @property
+    def is_connected(self) -> bool:
+        """Whether the Bluetooth speaker is currently connected."""
+        return self.client.is_connected
+
+    async def connect(self) -> None:
+        """Attempt to connect to the Bluetooth device."""
+        await self.client.connect()
+
+    async def disconnect(self) -> None:
+        """Disconnect from the Bluetooth device."""
+        await self.client.disconnect()
+
+    async def update(self) -> None:
+        pass
+
+
+class BluetoothSpeakerCoordinator(DataUpdateCoordinator[None]):
     """Class to handle connection to the bluetooth speaker."""
-    pass
+    device: BluetoothSpeakerDevice
+    config_entry: BluetoothSpeakerConfigEntry
+
+    def __init__(self, hass: HomeAssistant, entry: BluetoothSpeakerConfigEntry) -> None:
+        """Initialize the coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            config_entry=entry,
+            name=DOMAIN,
+            update_interval=timedelta(seconds=DEFAULT_SCAN_INTERVAL),
+        )
+
+    async def _async_setup(self) -> None:
+        """Set up the coordinator."""
+        address = self.config_entry.unique_id
+
+        assert address is not None
+
+        ble_device = bluetooth.async_ble_device_from_address(
+            self.hass, address)
+
+        if not ble_device:
+            raise ConfigEntryNotReady(
+                f"Could not find Bluetooth device with address {address}"
+            )
+
+        self.device = BluetoothSpeakerDevice(ble_device)
+
+    async def _async_update_data(self) -> None:
+        """Update status of Bluetooth speaker."""
+        try:
+            await self.device.update()
+        except Exception as err:
+            raise UpdateFailed(f"Unable to fetch data: {err}") from err
